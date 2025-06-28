edd4736061b00527e183d57c60e889296d834d33
Robin Gruenke <eimfach@me.com>
2025-06-28 04:21:31 +0200

    custom build step for vs

--- task/compile_shaders.bat
+++ task/compile_shaders.bat
@@ -1,5 +1,4 @@
-glslc.exe --target-env=vulkan1.0 --target-spv=spv1.0 ..\shaders\simple_shader.vert -o ..\shaders\simple_shader.vert.spv
-glslc.exe --target-env=vulkan1.0 --target-spv=spv1.0 ..\shaders\simple_shader.frag -o ..\shaders\simple_shader.frag.spv
-spirv-dis.exe ..\shaders\simple_shader.vert.spv
-spirv-dis.exe ..\shaders\simple_shader.frag.spv
-pause
+glslc.exe --target-env=vulkan1.0 --target-spv=spv1.0 .\shaders\simple_shader.vert -o .\shaders\simple_shader.vert.spv
+glslc.exe --target-env=vulkan1.0 --target-spv=spv1.0 .\shaders\simple_shader.frag -o .\shaders\simple_shader.frag.spv
+REM spirv-dis.exe ..\shaders\simple_shader.vert.spv
+REM spirv-dis.exe ..\shaders\simple_shader.frag.spv
