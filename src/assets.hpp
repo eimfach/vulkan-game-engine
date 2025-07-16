23a40d5718da890f740c18e326771b865b09b20f
Robin Gruenke <eimfach@me.com>
2025-07-16 15:50:54 +0200

    integrating assimp for flexible model formats;linking all libs as static

---
+++ src/assets.hpp
@@ -0,0 +1,7 @@
+#include <assimp/Importer.hpp>      // C++ importer interface
+#include <assimp/scene.h>           // Output data structure
+#include <assimp/postprocess.h>     // Post processing flags
+
+namespace SJFGame::Assets {
+    bool import_model(const std::string& filepath);
+}
